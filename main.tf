cb8f9400a6e234e6c21ba2faf30f75fcdc294553
jeanmathew3121 <jeanmathew3121@gmail.com>
2023-05-12 05:24:48 +0100

    ini

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    checkpoint = {
+      source  = "checkpointsw/checkpoint"
+      version = "~> 1.6.0"
+    }
+  }
+}
+
+# Configure the Check Point Provider
+provider "checkpoint" {
+  server   = var.cp-mgmt-server
+  username = var.cp-mgmt-username
+  password = var.cp-mgmt-password
+  context  = var.cp-mgmt-context
+  session_name = "terraform_session"
+}
+
+data "checkpoint_management_data_access_rule" "data_access_rule"{
+  name = "Cleanup rule"
+  layer = "Network"
+}
+
+output "instance_ip_addr" {
+  value = data.checkpoint_management_data_access_rule.data_access_rule.action
+}
+
+
+resource "checkpoint_management_access_rule" "rule" {
+  layer = "Network"
+  position = {above = data.checkpoint_management_data_access_rule.data_access_rule.name}
+  name = var.cp-name
+  action = var.cp-action
+  //action_settings = {
+  //  enable_identity_captive_portal = true
+  //}
+  source = [var.cp-source-networks]
+  enabled = true
+  destination = [var.destination-networks]
+  //destination_negate = true
+  service = [var.cp-service]
+}
+
+
+resource "checkpoint_management_publish" "publish" { 
+  depends_on = [
+    checkpoint_management_access_rule.add-rule
+  ]
+}
+resource "checkpoint_management_logout" "logout" {
+  depends_on = [
+    checkpoint_management_publish.publish
+ ]
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "cp-mgmt-server" {
+ 
+}
+
+variable "cp-mgmt-username" {
+ 
+}
+
+variable "cp-mgmt-password" {
+ 
+}
+variable "cp-mgmt-context" {
+ 
+}
+
+variable "cp-service" {
+ 
+}
+
+variable "cp-source-networks"{ 
+}
+variable "cp-destination-networks"{ 
+}
+variable "cp-name" {
+ 
+}
+variable "cp-action" {
+ 
+}
